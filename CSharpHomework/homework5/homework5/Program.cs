99b0f4a03d2cefc8338ce7a35a7da9ea15a7c62f
Leona1226 <1124691146@qq.com>
2018-10-22 23:37:02 +0800

    homework6

--- CSharpHomework/homework5/homework5/Program.cs
+++ CSharpHomework/homework5/homework5/Program.cs
@@ -11,10 +11,10 @@ namespace homework5
         static void Main(string[] args)
         {
             OrderService os = new OrderService();
-            os.AddOrder(1, "LeoZB", "Leona");
-            os.AddOrder(2, "JohnnyZB", "wotslb");
-            os.RemoveOrder(2, "JohnnyZB", "wotslb");
-            os.AmendOrder(1, "LeoZB", "Leona", 2, "JohnnyZB", "wotslb");
+            os.AddOrder(1, "LeoZB", "Leona",1000);
+            os.AddOrder(2, "JohnnyZB", "wotslb",2000);
+            os.RemoveOrder(2, "JohnnyZB", "wotslb",2000);
+            os.AmendOrder(1, "LeoZB", "Leona",1000, 2, "JohnnyZB", "wotslb",2000);
             os.InquireByCustomer("wotslb");
             os.InquireByNum(1);
             os.InquireByName("JohnnyZB");
