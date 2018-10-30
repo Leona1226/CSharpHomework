da84727d58facbb42ce3de4ca7482a992f5aac28
Leona1226 <1124691146@qq.com>
2018-10-30 11:54:57 +0800

    homework7

---
+++ CSharpHomework/homework7/homework7-2/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using homework7;
+
+namespace homework7_2
+{
+    public partial class Form1 : Form
+    {
+        OrderService os = new OrderService();
+        OrderDetails od = new OrderDetails();
+        public Form1()
+        {
+            InitializeComponent();
+            os.AddOrder(1, "LeoZB", "Leona", 1000);
+            os.AddOrder(2, "JohnnyZB", "wotslb", 2000);
+            orderDetailsBindingSource.DataSource = os.list;
+            //绑定查询条件
+            textBox1.DataBindings.Add("Text", od, "orderNum");
+            textBox2.DataBindings.Add("Text", od, "ProductName");
+            textBox3.DataBindings.Add("Text", od, "Customer");
+            textBox4.DataBindings.Add("Text", od, "sum");
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            orderDetailsBindingSource.DataSource = os.list.Where(s => s.OrderNum == od.OrderNum);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            orderDetailsBindingSource.DataSource = os.list.Where(s => s.Customer == od.Customer);
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            orderDetailsBindingSource.DataSource = os.list.Where(s => s.ProductName == od.ProductName);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            orderDetailsBindingSource.DataSource = os.list.Where(s => s.sum == od.sum);
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.ShowDialog();
+            os.AddOrder(f2.f1(), f2.f2(), f2.f3(), f2.f4());
+            orderDetailsBindingSource.DataSource = os.list.Where(s => s.OrderNum >= 0);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Form3 form3 = new Form3();
+            form3.ShowDialog();
+            os.AmendOrder(form3.f1(), form3.f2(), form3.f3(), form3.f4(), form3.f5(), form3.f6(), form3.f7(), form3.f8());
+            orderDetailsBindingSource.DataSource = os.list.Where(s => s.OrderNum >= 0);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.ShowDialog();
+            os.RemoveOrder(form2.f1(), form2.f2(), form2.f3(), form2.f4());
+            orderDetailsBindingSource.DataSource = os.list.Where(s => s.OrderNum >= 0);
+        }
+    }
+}
---
+++ CSharpHomework/homework7/homework7-2/Form2.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace homework7_2
+{
+    public partial class Form2 : Form
+    {
+        public int num { set; get; }
+        public String name { set; get; }
+        public String customer { set; get; }
+        public int sum { set; get; }
+        public Form2()
+        {
+            InitializeComponent();
+            textBox1.DataBindings.Add("Text", this, "num");
+            textBox2.DataBindings.Add("Text", this, "name");
+            textBox3.DataBindings.Add("Text", this, "customer");
+            textBox4.DataBindings.Add("Text", this, "sum");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        public int f1()
+        {
+            return num;
+        }
+        public string f2()
+        {
+            return name;
+        }
+        public string f3()
+        {
+            return customer;
+        }
+        public int f4()
+        {
+            return sum;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
